ac30ab576780927e0d83a44782add7fbfda45727
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2024-03-05 12:10:21 +0900

    feat: add button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/shadcn/Button";
+
 function App() {
   return (
     <div>
       <header className="font-extrabold text-2xl">hi</header>
+      <Button>Hello</Button>
     </div>
   );
 }
